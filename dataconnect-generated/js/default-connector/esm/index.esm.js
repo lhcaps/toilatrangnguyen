582e6edf892ff14020c2bb758d896ec0f1305163
lhcaps <huyle210525@gmail.com>
2025-06-20 16:27:37 +0700

    Initial commit - Add full project

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Tôi là trạng nguyên web',
+  location: 'asia-east1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'Tôi là trạng nguyên web',
+  location: 'asia-east1'
+};
+exports.connectorConfig = connectorConfig;
